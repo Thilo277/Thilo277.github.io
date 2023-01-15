6c2317359dfe7c501ddac86f7fae8258228e3703
Thilo277 <thilo2707@gmail.com>
2023-01-15 15:45:32 +0100

    general updates

--- ecochart/index.js
+++ ecochart/index.js
@@ -1,11 +1,17 @@
-/* var selected; */ // not needed anymore because the selected customer gets stored in localStorage from local function variable, to prevent resetting variable when loading main.html
-var back = 0; // Back button, warn-variable in case of misclick
-var hide = 0; // Hide/Export button, warn-variable in case of misclick
+// ------------------------------------- edit here --------------------------------------
+
 const firmen = ["Firma1", "Firma2", "Firma3", "Firma4"]; // Customers Array
 const text = ["Text1", "Text2", "Text3", "Text4", "Text5", "Text6", "Text7", "Text8", "Text9"]; // First Text array
 const text2 = ["Text1-2", "Text2-2", "Text3-2", "Text4-2", "Text5-2", "Text6-2", "Text7-2", "Text8-2", "Text9-2"]; // Second Text array
-
 var num = 1.0; // Number
+const buttons = '<button onclick="addrow1(0)">Text1</button><button onclick="addrow1(2)">Text2</button><button onclick="addrow1(3)">Text3</button><button onclick="addrow1(4)">Text4</button>'; // edit the buttons here, make sure to also do so in main.html!
+
+// -------------------------------- start of actual code --------------------------------
+
+/* var selected; */ // not needed anymore because the selected customer gets stored in localStorage from local function variable, to prevent resetting variable when loading main.html
+var back = 0; // Back button, warn-variable in case of misclick
+var hide = 0; // Hide/Export button, warn-variable in case of misclick
+
 
 function start() {
     var selected = customerselector.selectedIndex; // look up which customer is selected
@@ -15,18 +21,18 @@ function start() {
 
 function addrow1(number) {
     tbody.innerHTML += `
-    <td>${num.toFixed(1)}</td>
+    <td>${num.toFixed(2)}</td>
     <td><input type="date"></td>
     <td><textarea>${text[number]}</textarea></td>
-    <td><textarea></textarea></td>
+    <td><input type="text"></td>
     <td><input type="checkbox"></td>
-    <td id="id${num.toFixed(1)}"><input onclick="makered('id${num.toFixed(1)}')" type="radio" name="class${num.toFixed(1)}"><input onclick="makeyellow('id${num.toFixed(1)}')" type="radio" name="class${num.toFixed(1)}"><input onclick="makegreen('id${num.toFixed(1)}')" type="radio" name="class${num.toFixed(1)}"></td>
+    <td id="id${num.toFixed(2)}"><input onclick="makered('id${num.toFixed(2)}')" type="radio" name="class${num.toFixed(2)}"><input onclick="makeyellow('id${num.toFixed(2)}')" type="radio" name="class${num.toFixed(2)}"><input onclick="makegreen('id${num.toFixed(2)}')" type="radio" name="class${num.toFixed(2)}"></td>
     <td><textarea>${text2[number]}</textarea></td>
-    <td><img id="image${num.toFixed(1)}" width="100%"><input id="fileinput" type="file" onchange="document.getElementById('image${num.toFixed(1)}').src = window.URL.createObjectURL(this.files[0])"></td>
+    <td><img id="image${num.toFixed(2)}" width="100%"><input id="fileinput" type="file" onchange="document.getElementById('image${num.toFixed(2)}').src = window.URL.createObjectURL(this.files[0])"></td>
     <td><input type="month"></td>
-    <td><input type="date"><textarea></textarea></td>
+    <td><input type="date"><input type="text"></td>
     `; // add row to table
-    num = num + 0.1; // count number up
+    num = num + 0.01; // count number up
 }
 
 function makered(id) {
@@ -77,8 +83,8 @@ function load() {
 
 function zuruck() {
     if (back == 0) {
-        alert('Sind sie sicher, dass sie zurück zum Hauptmenü wollen? Alle nicht exportierten Änderungen gehen verlohren! Falls ja, nochmal auf zurück drücken.'); // go back, ask in case of misclick
-        back++;
+        alert('Sind sie sicher, dass sie zurück zum Hauptmenü wollen? Alle nicht exportierten Änderungen gehen verlohren! Falls ja, nochmal auf zurück drücken.'); 
+        back++; // go back, ask in case of misclick
     }
     else if (back > 0) {
         back = 0;
@@ -90,22 +96,31 @@ function zuruck() {
 
 function hidebuttons() {
     if (hide == 0) {
-        alert('Sind sie sicher, dass sie die Steuerflächen ausblenden und diese Tabelle Exportieren wollen? Diese werden erst in 1min wieder eingeblendet! Falls ja, nochmal auf Knöpfe ausblenden und Exportieren drücken.');
+        alert('Sind sie sicher, dass sie die Steuerflächen ausblenden und diese Tabelle Exportieren wollen? Die Steuerelemente werden in 1min wieder angezeigt. Falls ja, nochmal auf Exportieren drücken.');
         hide++; // hide and export, ask in case of misclick
     }
     else if (hide > 0) {
         hide = 0;
         btns.innerHTML = '';
         print();
-        setTimeout(function () {
-            btns.innerHTML = `<button onclick="zuruck()">Zurück</button><button onclick="hidebuttons()">Knöpfe ausblenden und Exportieren</button>
-            <br>
-            <br>
-            <br>
-            <button onclick="addrow1(0)">Text1</button><button onclick="addrow1(2)">Text2</button><button onclick="addrow1(3)">Text3</button><button onclick="addrow1(4)">Text4</button>`;
-        }, 60000); // go hide and export
+        setTimeout(addbuttons, 60000); // go hide and export
     }
 
+}
+
+function addbuttons() {
+    btns.innerHTML = `<button onclick="zuruck()">Zurück</button><button onclick="hidebuttons()">Exportieren</button><button onclick="newcat()">Neue Kategorie</button>
+    <br>
+    <br>
+    <br>
+    ${buttons}`; // Make the buttons easier to edit, see line 1
+}
+
+function newcat()
+{
+    parseInt(num, 10);
+    num = num + 1;
+    tbody.innerHTML += '<td colspan="10" class="category"><input type="text" class="categoryinput" value="'+ num +' "></td>';
 }
 
 
